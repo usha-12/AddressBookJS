240689fad1de1754cb2a8734288e09f4944084e7
usha-12 <usha12ahirwar@gmail.com>
2022-11-30 11:26:23 +0530

    Ability to create a address book contact

---
+++ AddressBook.js
@@ -0,0 +1,27 @@
+console.log("------------------------------Welcome to the Address book program-----------------------------");
+class AddressBook {
+
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNumber;
+    email;
+
+    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    toString() {
+        return "First name: " + this.firstName + "\nLast name: " + this.lastName + "\nAddress: " + this.address + "\nCity: " + this.city + "\nState: " + this.state + "\nZip: " + this.zip + "\nPhone number: " + this.phoneNumber + "\nEmail: " + this.email;
+    }
+}
